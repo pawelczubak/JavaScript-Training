e427e099654a8708e9dd89ec3ee72685b2652f68
Paweł <pawelczubak@op.pl>
2020-06-09 10:36:03 +0100

    training

---
+++ .filter/main.js
@@ -0,0 +1,12 @@
+const numbers = [1, 3, 5, -3, -7]
+
+// const filtered = numbers.filter(function (e) {
+//     return e >= 0
+// })
+
+
+// arrow functiom
+
+const filtered = numbers.filter(e => e >= 0)
+
+console.log(filtered)
